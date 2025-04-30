1cc34f62080f3677742b964d1a73ff1d4263899e
Vlad Kolhanov <vladi.kolganov@gmail.com>
2025-04-30 13:45:53 +0300

    chore: create monorepo and extract ESLint, Prettier, and TypeScript configs into separate packages

---
+++ packages/configs/eslint/base.js
@@ -0,0 +1,79 @@
+import * as path from 'node:path'
+import { includeIgnoreFile } from '@eslint/compat'
+import js from '@eslint/js'
+import prettierConfig from 'eslint-config-prettier'
+import * as regexpPlugin from 'eslint-plugin-regexp'
+import pluginSecurity from 'eslint-plugin-security'
+import tseslint from 'typescript-eslint'
+import turboPlugin from 'eslint-plugin-turbo'
+
+export default tseslint.config(
+  includeIgnoreFile(path.join(import.meta.dirname, '../../../.gitignore')),
+  { ignores: ['**/*.config.*'] },
+  {
+    files: ['**/*.js', '**/*.ts', '**/*.tsx'],
+    plugins: {
+      turbo: turboPlugin,
+    },
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+      regexpPlugin.configs['flat/recommended'],
+      pluginSecurity.configs.recommended,
+      prettierConfig,
+    ],
+    rules: {
+      ...turboPlugin.configs.recommended.rules,
+
+      // JS rules
+      eqeqeq: 'error',
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-duplicate-imports': 'error',
+      'no-eval': 'error',
+      'no-template-curly-in-string': 'error',
+      'no-unreachable': 'error',
+      'no-useless-catch': 'error',
+      'no-var': 'error',
+      'prefer-template': 'error',
+
+      // Typescript rules
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/array-type': 'error',
+      '@typescript-eslint/consistent-indexed-object-style': 'error',
+      '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
+      '@typescript-eslint/no-unnecessary-qualifier': 'error',
+      '@typescript-eslint/no-unnecessary-template-expression': 'error',
+      '@typescript-eslint/no-unnecessary-type-arguments': 'error',
+      '@typescript-eslint/no-useless-empty-export': 'error',
+      '@typescript-eslint/prefer-literal-enum-member': 'error',
+      '@typescript-eslint/prefer-reduce-type-parameter': 'error',
+      '@typescript-eslint/prefer-string-starts-ends-with': 'error',
+      '@typescript-eslint/promise-function-async': 'error',
+      '@typescript-eslint/unified-signatures': 'error',
+      '@typescript-eslint/use-unknown-in-catch-callback-variable': 'error',
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        { fixStyle: 'inline-type-imports' },
+      ],
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        { checksVoidReturn: { attributes: false } },
+      ],
+      '@typescript-eslint/no-unnecessary-condition': [
+        'error',
+        {
+          allowConstantLoopConditions: true,
+        },
+      ],
+    },
+  },
+  {
+    linterOptions: { reportUnusedDisableDirectives: true },
+    languageOptions: { parserOptions: { projectService: true } },
+  }
+)
---
+++ packages/configs/prettier/index.js
@@ -0,0 +1,12 @@
+/** @typedef {import("prettier").Config} PrettierConfig */
+
+/** @type { PrettierConfig } */
+const config = {
+  printWidth: 80,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+}
+
+export default config
