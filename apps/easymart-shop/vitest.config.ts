467201757ee4cc92af6fd71f7f53041a6df74fc5
Vlad Kolhanov <vladi.kolganov@gmail.com>
2025-06-12 12:59:20 +0300

    fix: bug with resolve aliases in easymart-shop

--- apps/easymart-shop/vitest.config.ts
+++ apps/easymart-shop/vitest.config.ts
@@ -1,8 +1,14 @@
 import { defineProject } from 'vitest/config'
+import path from 'node:path'
 
 export default defineProject({
   test: {
     environment: 'jsdom',
     setupFiles: './vitest.setup.ts',
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './app'),
+    },
   },
 })
